c2c4cf6d365ca512994c074fd277d975323c88ea
Dipak4064 <dipaktamangbca2080@nepalaya.edu.np>
2025-09-23 19:06:11 +0545

    activity pushed

---
+++ DailyActivity/Laravel/migration_practice/database/migrations/2025_09_23_015034_create_students_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->Integer('student_id');
+            $table->string('name');
+            $table->string('email')->unique()->nullable();
+            $table->float('percentage', 3, 2)->comment('student percentage');
+            $table->primary('student_id');
+            $table->string('city')->default('No City');
+            $table->integer('age')->unsigned();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('students');
+    }
+};
---
+++ DailyActivity/Laravel/migration_practice/database/migrations/2025_09_23_121836_create_libraries_table.php.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('libraries', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('em_id');
+            $table->foreign('em_id')->references('id')->on('employe')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->string('book');
+            $table->date('due_date')->nullable();
+            $table->boolean('status');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
